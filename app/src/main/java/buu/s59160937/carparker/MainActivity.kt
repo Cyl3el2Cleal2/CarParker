3a6f913a3dbb13d4ed29f6d624238ae4e0e5e164
cyl3el2cleal2 <nathachian@gmail.com>
2019-09-24 23:50:58 +0700

    Add fragment

--- app/src/main/java/buu/s59160937/carparker/MainActivity.kt
+++ app/src/main/java/buu/s59160937/carparker/MainActivity.kt
@@ -2,11 +2,15 @@ package buu.s59160937.carparker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import buu.s59160937.carparker.R.layout.activity_main
+import buu.s59160937.carparker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
